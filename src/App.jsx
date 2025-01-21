cf8dc45da424729c2afb44848bbd4f839f76c48f
Parth Patidar <parth1207patidar@gmail.com>
2025-01-21 15:27:23 +0530

    Initial prototype

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./components/dashboard"
+
+const App = () => {
+  return (
+    <div className="dark:bg-[#424242] dark:text-white">
+      <Dashboard></Dashboard>
+    </div>
+  )
+}
+
+export default App
